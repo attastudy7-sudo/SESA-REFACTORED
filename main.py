a88d7368238208ee5d5bcd5816def861979e5588
Alan Focus <attastudy7@gmail.com>
2026-05-16 14:00:01 +0000

    Downtime

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
-# import os
-# from app import create_app
+import os
+from app import create_app
 
-# app = create_app(os.environ.get('FLASK_ENV', 'development'))
+app = create_app(os.environ.get('FLASK_ENV', 'development'))
 
-# if __name__ == '__main__':
-#     app.run(
-#         host='0.0.0.0',
-#         port=int(os.environ.get('PORT', 5000)),
-#         debug=True
-#     )
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000)),
+        debug=True
+    )
